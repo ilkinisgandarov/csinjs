de135369f70fc3c088c26761c488539b7b04f849
Ilkin Isgandarov <ilkin.isgandarov@gmail.com>
2018-12-12 12:27:40 +0400

    test changed

--- algorithms/sorting/selection-sort/solution.test.js
+++ algorithms/sorting/selection-sort/solution.test.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const selectionSort = require('./solution');
 
 describe('Testing selection sort', () => {
-    it('Sorting result should equal [2,3,4,5,5,9,12]', () => {
-        expect(selectionSort([9, 4, 2, 5, 3, 5, 12])).to.be.deep.equal([2, 3, 4, 5, 5, 9, 12]);
+    it('Sorting result should equal [2,3,4,5,5,9,12,1]', () => {
+        expect(selectionSort([9, 4, 2, 5, 3, 5, 12, 1])).to.be.deep.equal([1, 2, 3, 4, 5, 5, 9, 12]);
     })
 })

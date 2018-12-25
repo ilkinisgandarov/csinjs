fc4b01b812b60f7b2b02ff4c2b9bd440b628c996
Cafarov Mikayil <cafarovmikayil95@gmail.com>
2018-12-25 10:41:04 +0400

    solution-3 added (with test) to first-missing-positive problem

--- problems/first-missing-positive/solution-3.js
+++ problems/first-missing-positive/solution-3.js
@@ -3,14 +3,37 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = function (nums, minVal = 1) {
-    if (!nums.length) return minVal;
-
+function firstMissingPositive(nums) {
+    if (!nums.length) return 1;
+  
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === minVal) return firstMissingPositive(nums, ++minVal);
-
-        if ((i === nums.length - 1) && nums[i] !== minVal) return minVal;
+      let val = nums[i];
+  
+      if (val <= 0) {
+        nums[i] = null;
+  
+        continue;
+      }
+  
+      if (i !== val - 1) {
+        if (nums[val - 1]) {
+          if (nums[i] === nums[val - 1]) nums[i] = null;
+          else {
+            nums[i] = nums[val - 1];
+            --i;
+          }
+        } else {
+          nums[i] = null;
+        }
+  
+        nums[val - 1] = val;
+      }
     }
-};
+  
+    for (let i = 0; i < nums.length; i++) {
+      if (!nums[i]) return i + 1;
+      else if (i === nums.length - 1) return nums[i] + 1;
+    }
+  }
 
 module.exports.firstMissingPositive = firstMissingPositive;
